46b1457e65f505f5bc176d5399b68b603b9abf16
Brandon <brandon-gallardo@live.com>
2016-11-28 23:10:10 -0300

    Add necesary driver for the connection

--- src/java/bd/ConexionBD.java
+++ src/java/bd/ConexionBD.java
@@ -3,6 +3,8 @@ package bd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexionBD {
     
@@ -15,9 +17,13 @@ public class ConexionBD {
     public static Connection getConexion() {
         
         try {
-            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/inacap", "inacap", "inacap");           
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/inacap", "inacap", "inacap");   
+            return connection;
         } catch(SQLException e) {
             System.out.println("Error de conexión de base de datos");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         return null;
