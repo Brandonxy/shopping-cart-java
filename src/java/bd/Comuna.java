0800253173c6db26c8d93cf89134d54228693526
Brandon <brandon-gallardo@live.com>
2016-12-06 01:33:02 -0300

    Add model for Pais, region, comuna

---
+++ src/java/bd/Comuna.java
@@ -0,0 +1,28 @@
+package bd;
+
+public class Comuna extends Conexion {
+    
+    private int id;
+    private String nombre;
+
+    public Comuna(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
