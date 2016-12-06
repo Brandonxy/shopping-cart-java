bc10765c2a962e422146df08376d471cb05c04da
Brandon <brandon-gallardo@live.com>
2016-12-06 01:23:44 -0300

    Helper for generating urls
    This helper return a link prefixed with the root path of the project

---
+++ src/java/bd/Url.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+/**
+ *
+ * @author unknown
+ */
+public class Url {
+
+    public Url() {
+        
+    }
+    
+    public static String linkTo(String url)
+    {
+        return "/TallerProgramacion2/" + url;
+    }
+}
