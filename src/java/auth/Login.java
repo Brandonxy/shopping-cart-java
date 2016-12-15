f4d30f63441fc41eac9a8749ff6a821585d8ee2e
Brandon <brandon-gallardo@live.com>
2016-12-15 12:49:02 -0300

    Replace jsp login post with Servlet

---
+++ src/java/auth/Login.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auth;
+
+import bd.ConexionBD;
+import bd.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author unknown
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String run = request.getParameter("run");
+            String pass = request.getParameter("password");
+
+            HttpSession hs = request.getSession(true);
+
+            try
+            {
+                Connection conn = ConexionBD.getConexion();
+
+                String sql      = "select * from inacap.Clientes where run=? and password=? ";
+                PreparedStatement pst = conn.prepareStatement(sql);
+                pst.setString(1, run);
+                pst.setString(2, pass);
+                ResultSet rs = pst.executeQuery();
+                //out.println(run + pass);
+                // Validar los datos recién recuperados.
+                if( rs.next() )
+                {
+                    ResultSet user = User.find("clientes", rs.getLong("id"));
+                    user.next();
+                
+                    hs.setAttribute("User", new User(
+                                                user.getLong("id"), 
+                                                user.getString("nombre"), 
+                                                user.getString("apellido"),
+                                                user.getString("run"), 
+                                                user.getInt("edad"), 
+                                                user.getString("telefono")));
+                    response.sendRedirect("administrador/BienvenidaAdmin.jsp");
+                }
+                else
+                {
+                    out.println("Error: usuario o clave erróneos.");
+                    out.println("<br />");
+                    out.println("<a href='login.jsp'>Volver al ingreso</a>");
+                }
+            }
+            catch (SQLException e)
+            {
+                out.println("Excepción de SQL: " + e);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
