b48a53e0c1e7637b6bfede4dfd82ed5ced638ebe
Brandon <brandon.gallardo@outlook.com>
2016-12-30 15:18:34 -0300

    Remove unused method

--- src/java/auth/CloseSession.java
+++ src/java/auth/CloseSession.java
@@ -49,21 +49,6 @@ public class CloseSession extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     /**
      * Returns a short description of the servlet.
      *
